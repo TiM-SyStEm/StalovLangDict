c0d1b787ebb69457851e108ff1446b4708b343dc
Timofei Gorlov <72683799+TiM-SyStEm@users.noreply.github.com>
2022-02-08 13:13:30 +0700

    v1.1

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+"use strict";
+
 function translST_RU(){
-	alert("test");
+  let obj = document.getElementById("main_input");
+  let text = obj.value;
+  console.log("text input: " + text);
+  let words = text.split(" ");
+  let length = words.length;
+  console.log(length);
 }
